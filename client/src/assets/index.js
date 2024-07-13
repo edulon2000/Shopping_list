ab1a63bea0bf6dd424cb9bed1e6c96a4cf75b4de
edulon2000 <edulon2015@gmail.com>
2024-07-12 23:31:50 -0300

    Initial Commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+
+const assets = {
+    images:{
+        logoDark: require('./imagens/logoDark.png'),
+        appIcon: require('./imagens/favicon.png')
+    },
+    colors: {
+        secondary: '#292929'
+    }
+}
+export default assets
